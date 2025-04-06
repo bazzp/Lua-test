fa243b88767d1786c7b3b801cc1494f4938a9dff
Jakub Ujma <143741774+bazzp@users.noreply.github.com>
2025-04-06 23:21:24 +0200

    square

--- main.lua
+++ main.lua
@@ -1,10 +1,38 @@
-function love.load()
-    love.graphics.setBackgroundColor(0.2, 0.6, 0.8) -- kolor tła (RGB w skali 0–1)
-    font = love.graphics.newFont(32) -- czcionka 32px
-    love.graphics.setFont(font)
+local angle = 0  
+local x, y = 400, 300  
+local size = 100  
+
+local rotationSpeed = 5  
+local maxspeed= 20;
+local minspeed= -20;
+
+
+function love.update(dt)
+   
+    if love.keyboard.isDown("up") then
+        rotationSpeed = math.min(rotationSpeed + 1, maxspeed)
+    end
+
+    
+    if love.keyboard.isDown("down") then
+        rotationSpeed = math.max(rotationSpeed - 1, minspeed)
+    end
+
+    angle = angle + rotationSpeed * dt
+    
+    if love.keyboard.isDown("escape") then
+        love.event.quit()
+    end
 end
 
 function love.draw()
-    love.graphics.setColor(1, 1, 1) -- biały tekst
-    love.graphics.print("Siema Lua!", 100, 100)
+    
+    love.graphics.push()  
+    love.graphics.translate(x, y)  
+    love.graphics.rotate(angle)  
+
+    
+    love.graphics.rectangle("line", -size / 2, -size / 2, size, size)
+
+    love.graphics.pop() 
 end
